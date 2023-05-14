49276c8d83695076d947aeb8f1ca9a3cc82a5058
anna-khoroshavina <a.s.khoroshavina@gmail.com>
2023-05-14 19:51:04 +0200

    clear DB on restart

--- src/main/java/com/example/journal/service/UserService.java
+++ src/main/java/com/example/journal/service/UserService.java
@@ -5,6 +5,7 @@ import com.example.journal.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -12,14 +13,11 @@ public class UserService {
     private final UserRepository repository;
 
     @Autowired
-    public UserService(UserRepository userRepository) {this.repository = userRepository;}
+    public UserService(UserRepository userRepository) {
+        this.repository = userRepository;
+    }
 
     public List<User> getAll() {
-        return List.of(
-                new User(1L, "Студент"),
-                new User(2L, "Преподователь")
-        );
-
-
+        return Collections.emptyList();
     }
 }
